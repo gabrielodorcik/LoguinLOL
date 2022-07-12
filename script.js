2de91b14e25670b2c1f2ba8b8b1e66d3857f8a69
Gabriel Odorcik <gabrielodorcik@gmail>
2022-07-12 15:46:46 -0300

    Tela de login do LOL

---
+++ script.js
@@ -0,0 +1,44 @@
+const inputs = domument.querySelectorAll('.input');
+const button = document.querySelector('.login__button');
+
+const handleFocus = ({ target }) => {
+
+    if(target.value === '') {
+
+        const span = target.previousElementSibling;
+        span.classList.add('span-active');
+
+    }
+
+}
+
+const handleFocusOut = ({ target }) => {
+
+    if(target.value === '') {
+
+        const span = target.previousElementSibling;
+        span.classList.remove('span-active');
+
+    }
+
+}
+
+const handleChange = () => {
+
+    const [username, password] = inputs;
+
+    if (username.value && password.value.lenght >= 8){
+
+        button.removeAttribute('disabled');
+
+    } else {
+
+        button.setAttribute('disabled', '');
+
+    }
+
+}
+
+inputs.forEach((input) => input.addEventListener('focus', handleFocus));
+inputs.forEach((input) => input.assEventListener('focusout', handleFocusOut));
+inputs.forEach((input) => input.addEventListener('input', handleChange));
